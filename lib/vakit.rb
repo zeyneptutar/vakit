c99d1c10d727259a90788faea13583e39a34f692
scary.mac <soner2@gmail.com>
2013-08-11 14:52:00 +0300

    Ready to publish

--- lib/vakit.rb
+++ lib/vakit.rb
@@ -1,6 +1,36 @@
 require "vakit/version"
 require 'vakit/connect'
+require 'Nokogiri'
+require 'open-uri'
 
 module Vakit
-  # Your code goes here...
+  
+  def self.today
+  	Vakit::Connect.shaber
+  end
+
+  def self.imsak
+  	Vakit::Connect.shaber[:imsak]
+  end
+
+  def self.sabah
+  	Vakit::Connect.shaber[:sabah]
+  end
+
+  def self.oglen
+  	Vakit::Connect.shaber[:oglen]
+  end
+
+  def self.ikindi
+  	Vakit::Connect.shaber[:ikindi]
+  end
+
+  def self.aksam
+  	Vakit::Connect.shaber[:aksam]
+  end
+
+  def self.yatsi
+  	Vakit::Connect.shaber[:yatsi]
+  end
+
 end
--- spec/connector_spec.rb
+++ spec/connector_spec.rb
@@ -2,8 +2,33 @@ require 'spec_helper'
 
 
 
-describe "Connector" do
+describe "Connector#new" do
 	let(:connector) { Vakit::Connect }
 	subject { connector }
 	it { should respond_to(:new)}
+
+
+	context "Conect to samanyoluhaber.com" do
+		let(:connector) { Vakit::Connect.shaber }
+		subject { connector }
+
+		describe "connected" do
+			it { should be_a(Hash)}
+			its(:length) { should == 6 }
+		end
+
+		describe "data" do
+			
+			it { should include(:imsak)}
+			it { should include(:sabah)}
+			it { should include(:oglen)}
+			it { should include(:ikindi)}
+			it { should include(:aksam)}
+			it { should include(:yatsi)}
+
+		end
+
+
+
+	end
 end
