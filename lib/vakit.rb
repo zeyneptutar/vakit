1032bcd2db46d6937c09f92f2b6af9beb82ff8db
zeyneptutar <z.tutar@gmail.com>
2014-06-09 12:47:46 +0300

    log message added

--- lib/vakit.rb
+++ lib/vakit.rb
@@ -45,24 +45,25 @@ module Vakit
   end
 
   def self.kerahat?
-    time=Time.now
-    sabah=Time.parse(Connect.shaber[:sabah])
-    aksam=Time.parse(Connect.shaber[:aksam])
-    kerahat_sabah=sabah + 45*60
-    kerahat_aksam=aksam - 45*60
+      time=Time.now
+      sabah=Time.parse(Connect.shaber[:sabah])
+      aksam=Time.parse(Connect.shaber[:aksam])
+      kerahat_sabah=sabah + 45*60
+      kerahat_aksam=aksam - 45*60
 
-    if Time.now > sabah && Time.now < kerahat_sabah
-        puts "Saat su an #{Time.now.strftime("%H:%M")}. Sabah kerahat vakti"
-      elsif  Time.now < aksam && Time.now > kerahat_aksam
-        puts "Saat su an #{Time.now.strftime("%H:%M")}. Aksam kerahat vakti"
-      else 
-        if self.prayer_time == Connect.shaber[:imsak]
-          puts "Saat su an #{Time.now.strftime("%H:%M")}. Kerahat vakti degil. Sabah kerahat vaktine kalan sure #{Time.diff(time, kerahat_sabah, '%h:%m')[:diff]}"
-        else
-          puts "Saat su an #{Time.now.strftime("%H:%M")}. Kerahat vakti degil. Ikindi kerahat vaktine kalan sure #{Time.diff(time, kerahat_aksam, '%h:%m')[:diff]}"
+      if Time.now > sabah && Time.now < kerahat_sabah
+          puts "Saat su an #{Time.now.strftime("%H:%M")}. Sabah kerahat vakti"
+        elsif  Time.now < aksam && Time.now > kerahat_aksam
+          puts "Saat su an #{Time.now.strftime("%H:%M")}. Aksam kerahat vakti"
+        else 
+          if self.prayer_time == Connect.shaber[:imsak]
+            puts "Saat su an #{Time.now.strftime("%H:%M")}. Kerahat vakti degil. Sabah kerahat vaktine kalan sure #{Time.diff(time, kerahat_sabah, '%h:%m')[:diff]}"
+          else
+            puts "Saat su an #{Time.now.strftime("%H:%M")}. Kerahat vakti degil. Ikindi kerahat vaktine kalan sure #{Time.diff(time, kerahat_aksam, '%h:%m')[:diff]}"
+        end
       end
-    end
-end
+      LOGGER.info("Kerahat vakti soruldu")
+  end
    
    def self.prayer_time 
      Connect.shaber.each_cons(2) do |(k,v), (k2,v2)|
